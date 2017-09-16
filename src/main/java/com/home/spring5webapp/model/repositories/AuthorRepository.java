6e5aa4eb99442ebdc00b53b716aa9e3a536daf0f
Ignat <ignat.khodasevich@gmail.com>
2017-09-16 16:11:58 +0300

    added controllers and templates

--- src/main/java/com/home/spring5webapp/model/repositories/AuthorRepository.java
+++ src/main/java/com/home/spring5webapp/model/repositories/AuthorRepository.java
@@ -3,8 +3,5 @@ package com.home.spring5webapp.model.repositories;
 import com.home.spring5webapp.model.Author;
 import org.springframework.data.repository.CrudRepository;
 
-/**
- * Created by Ignat on 14.09.17.
- */
 public interface AuthorRepository extends CrudRepository<Author, Long> {
 }
--- src/main/java/com/home/spring5webapp/model/repositories/PublisherRepository.java
+++ src/main/java/com/home/spring5webapp/model/repositories/PublisherRepository.java
@@ -3,8 +3,6 @@ package com.home.spring5webapp.model.repositories;
 import com.home.spring5webapp.model.Publisher;
 import org.springframework.data.repository.CrudRepository;
 
-/**
- * Created by Ignat on 16.09.17.
- */
+
 public interface PublisherRepository extends CrudRepository<Publisher, Long> {
 }
